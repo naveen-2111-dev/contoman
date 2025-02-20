6062780cf54dd973d8b1ff0ff11d1d2e2ceacb52
naveen-2111-dev <naveenrajanm9@gmail.com>
2025-02-20 22:11:03 +0530

    contoTest

---
+++ Contract/FuncCaller.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity ^0.8.0;
+import "./Test.sol";
+
+contract TestCaller {
+    TestFunc testFunc;
+
+    constructor(address _testFuncAddress) {
+        testFunc = TestFunc(_testFuncAddress);
+    }
+
+    function callAnyFunction(
+        address userContract,
+        string memory funcName,
+        bytes memory params
+    ) public returns (bytes memory) {
+        return testFunc.GetFunction(userContract, funcName, params);
+    }
+}
